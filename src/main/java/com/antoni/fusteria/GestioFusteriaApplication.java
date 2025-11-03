13ec4e6da7735bb86cfe4a1d5529ce4a1db01fc2
antoni.catany <antoni.catany@axisdata.net>
2025-11-03 15:19:20 +0100

    Projecte Final CFGS DAM

---
+++ src/main/java/com/antoni/fusteria/GestioFusteriaApplication.java
@@ -0,0 +1,13 @@
+package com.antoni.fusteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioFusteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioFusteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoni/fusteria/GestioFusteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoni.fusteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioFusteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
